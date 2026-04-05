ed3317b078f2159521dce0ae75887ee83d77b443
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-05 14:40:46 +0100

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let longestSubstring = ""
+
+        for (let i = 0; i < s.length; i++) {
+            // check odd palindromes
+            let p1 = i
+            let p2 = i
+            while (p1 >= 0 && p2 < s.length && s[p1] === s[p2]) {
+                if (((p2 - p1) + 1) > longestSubstring.length) {
+                    longestSubstring = s.substring(p1,p2+1)
+                }
+                p1--
+                p2++
+            }
+
+            // check even palindromes
+            p1 = i
+            p2 = i+1
+            while (p1 >= 0 && p2 < s.length && s[p1] === s[p2]) {
+                if (((p2 - p1) + 1) > longestSubstring.length) {
+                    longestSubstring = s.substring(p1,p2+1)
+                }
+                p1--
+                p2++
+            }
+        }
+
+        return longestSubstring
+    }
+}
