3a3dba63e4f6ee4c9e2ccf5f9cd23752b6c55bda
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-07 09:24:52 +0100

    Add: custom-sort-string - submission-0

---
+++ Data Structures & Algorithms/custom-sort-string/submission-0.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {string} order
+     * @param {string} s
+     * @return {string}
+     */
+    customSortString(order, s) {
+        const orderCount = new Map()
+        let permutationString = ""
+        let otherCharsString = ""
+
+
+        for (let i = 0; i < order.length; i++) {
+            orderCount.set(order[i], 0)
+        }
+
+        for (let i = 0; i < s.length; i++) {
+            if (orderCount.has(s[i])) {
+                let temp = orderCount.get(s[i])
+                temp += 1
+                orderCount.set(s[i], temp)
+            }
+        }
+
+        orderCount.forEach((val, key) => {
+            let p1 = 0
+
+            while (p1 < val) {
+                permutationString += key.toString()
+                p1++
+            }
+        })
+
+        for (let i = 0; i < s.length; i++) {
+            // otherCharsString += s[i]
+            if (!orderCount.has(s[i])) {
+                otherCharsString += s[i]
+            }
+        }
+
+        console.log("orderCount", orderCount)
+        console.log("permutaionString", permutationString)
+        console.log("otherCharsString", otherCharsString)
+
+        permutationString += otherCharsString
+
+        return permutationString
+    }
+}
