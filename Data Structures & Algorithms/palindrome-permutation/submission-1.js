ed5304f0abb53566f271633a6414f7749fcece53
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-14 21:00:36 +0100

    Add: palindrome-permutation - submission-1

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    canPermutePalindrome(s) {
+        const characters = new Map()
+        for (let i = 0; i < s.length; i++) {
+            if (characters.has(s[i])) {
+                let value = characters.get(s[i])
+                value++
+                characters.set(s[i], value)
+            } else {
+                characters.set(s[i], 1)
+            }
+        }
+
+        let odds = 0
+        characters.forEach((value, key) => {
+            if (value % 2 === 1) {
+                odds++
+            }
+        })
+
+        if ((s.length % 2 === 0 && odds === 0) || (s.length % 2 === 1 && odds === 1)) {
+            return true
+        }
+        return false
+    }
+}
