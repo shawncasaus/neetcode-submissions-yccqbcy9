8ca6c6efbe237c5fb07c6fdad85edb66e0f0a4c8
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-17 00:12:48 +0100

    Add: count-number-of-islands - submission-16

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-16.rs
@@ -0,0 +1,47 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn dfs( i: usize, 
+                j: usize, 
+                rows: usize, 
+                cols: usize, 
+                grid: &Vec<Vec<char>>,
+                visited: &mut HashSet<(usize, usize)>
+        ) {
+        if grid[i][j] == '0' || visited.contains(&(i,j)) {
+            return;
+        }
+
+        visited.insert((i,j));
+
+        if i > 0 {
+            Self::dfs(i-1,j, rows, cols, grid, visited);
+        }
+        if i < rows - 1 {
+            Self::dfs(i+1,j, rows, cols, grid, visited);
+        }
+        if j > 0 {
+            Self::dfs(i,j-1, rows, cols, grid, visited);
+        }
+        if j < cols - 1 {
+            Self::dfs(i,j+1, rows, cols, grid, visited);
+        }
+    }
+
+    pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+        let rows = grid.len();
+        let cols = grid[0].len();
+        let mut islands = 0;
+        let mut visited: HashSet<(usize, usize)> = HashSet::new();
+
+        for i in 0..rows {
+            for j in 0..cols {
+                if grid[i][j] == '1' && !visited.contains(&(i,j)) {
+                    islands += 1;
+                    Self::dfs(i,j, rows, cols, &grid, &mut visited);
+                }
+            }
+        }
+        islands
+    }
+}
