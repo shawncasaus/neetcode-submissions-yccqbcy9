fe26e7c36b1e522fc6b5ea87db91a0c82d82c9ff
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-18 22:10:31 +0100

    Add: count-number-of-islands - submission-20

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-20.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+        const visited = new Set();
+        let islands = 0
+
+        function dfs(i,j) {
+            if (i < 0 || i >= rows
+            || j < 0 || j >= cols
+            || visited.has(`[${i},${j}]`)
+            || Number(grid[i][j]) === 0) return;
+
+            visited.add(`[${i},${j}]`);
+            
+            dfs(i+1,j);
+            dfs(i-1,j);
+            dfs(i,j+1);
+            dfs(i,j-1);
+        }
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (Number(grid[i][j]) === 1 && !visited.has(`[${i},${j}]`)) {
+                    islands++;
+                    dfs(i,j);
+                }
+            }
+        }
+        return islands;
+    }
+}
