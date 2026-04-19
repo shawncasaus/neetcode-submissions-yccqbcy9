9c5a7359be95ae1d5d4a1aa9c14c07f07ce735d6
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-19 17:44:48 +0100

    Add: valid-word-square - submission-3

---
+++ Data Structures & Algorithms/valid-word-square/submission-3.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn valid_word_square(words: Vec<String>) -> bool {
+        let rows = words.len();
+        let mut col_map: HashMap<usize, String> = HashMap::new();
+
+        for i in 0..rows {
+            let curr_word_len = words[i].len();
+            for j in 0..curr_word_len {
+                if let Some(char) = words[i].chars().nth(j) {
+                    if let Some(value) = col_map.get_mut(&j) {
+                        value.push(char);
+                    } else {
+                        col_map.insert(j, char.to_string());
+                    }
+                }
+            }
+        }
+        println!("{:?}", col_map);
+        for i in 0..rows {
+            let curr_word = &words[i];
+            if let Some(value) = col_map.get_mut(&i) {
+                if curr_word != value {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+/**
+[ "ball",
+  "area",
+  "read",
+  "lady"]
+*/
