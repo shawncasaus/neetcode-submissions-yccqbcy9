1bed5c664c26957655b1ab053683026ee0a3a8e5
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-21 12:12:40 +0100

    Add: three-integer-sum - submission-16

---
+++ Data Structures & Algorithms/three-integer-sum/submission-16.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums = nums.sort((a,b) => a-b)
+        let sums = []
+        console.log(nums)
+        for (let i = 0; i < nums.length; i++) {
+            if (i>0 && nums[i] === nums[i-1]) continue;
+            let p1 = i+1
+            let p2 = nums.length-1
+            while (p1 < p2) {
+                const sum = nums[i] + nums[p1] + nums[p2]
+                if (sum > 0) {
+                    p2--
+                } else if (sum < 0) {
+                    p1++
+                } else {
+                    sums.push([nums[i], nums[p1], nums[p2]])
+                    p1++
+                    p2--
+                    while(nums[p1] === nums[p1-1]) p1++
+                    while(nums[p2] === nums[p2+1]) p2--
+                }
+            }
+        }
+        return sums
+    }
+}
