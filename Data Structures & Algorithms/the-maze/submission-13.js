e4687df882db88ab93d1d60a84dbe2e15d7f1f6c
Shawn Renee Casaus <shawn.casaus@gmail.com>
2026-04-24 13:56:45 +0100

    Add: the-maze - submission-13

---
+++ Data Structures & Algorithms/the-maze/submission-13.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[][]} maze
+     * @param {number[]} start
+     * @param {number[]} destination
+     * @return {boolean}
+     */
+    hasPath(maze, start, destination) {
+        const rows = maze.length
+        const cols = maze[0].length
+        const visited = new Set()
+
+        function dfs(i,j) {
+            if (visited.has(`[${i},${j}]`)) return false
+            if (destination[0] === i && destination[1] === j) return true
+
+            visited.add(`[${i},${j}]`)
+
+            const directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            for (let [di, dj] of directions) {
+                let r = i
+                let c = j
+                while ((r+di) >= 0 && (r+di) < rows
+                && (c+dj) >= 0 && (c+dj) < cols
+                && maze[r+di][c+dj] !== 1) {
+                    r += di
+                    c += dj
+                }
+                if (dfs(r,c)) return true
+            }
+            return false
+        }
+
+        return dfs(start[0], start[1])
+    }
+}
